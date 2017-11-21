e6c83dea5e8e11e52bfd0b6751f822888153dc08
Silvina Muñoz <silmunoz22@gmail.com>
2017-11-21 17:24:08 -0300

    footer icons and details #5

--- templates/footer.php
+++ templates/footer.php
@@ -5,23 +5,23 @@
 			<div class="col-xs-12 col-md-10 col-md-offset-1 col-sm-12">
 
 		<div class="col-md-4 col-xs-6 col-sm-2">
-			<h4>Encontranos en</h4>
-			<a href="http://donfranciscomaquinarias.com/ubicacion.php">
+			<h4 class="upper line">Encontranos en</h4>
+			<a href="ubicacion.php">
 				<figure>
 					<iframe width="300" height="200" class="iframe-footer" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=-38.532221,-58.769871&amp;num=1&amp;ie=UTF8&amp;t=h&amp;ll=-38.532187,-58.770504&amp;spn=0.030214,0.051413&amp;z=14&amp;output=embed"></iframe>
 				</figure>
 			</a>
 		</div>
 		<div class="col-md-4 col-xs-6 col-sm-6">
-			<h4>Buscanos en</h4>
+			<h4 class="upper line">Buscanos en</h4>
 				<iframe class="iframe-footer" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FDon-Francisco-Maquinarias%2F160607427288244&amp;width=300&amp;height=200&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=false&amp;appId=185172994932129" scrolling="no" frameborder="0" class="embed-responsive-item" allowTransparency="true"></iframe>
 			</div>
 			<div class="col-md-4 col-xs-12 col-sm-4">
-					<h4>Don Francisco</h4>
+					<h4 class="upper line">Don Francisco</h4>
 						<div class="textwidget">
 							<p>La  empresa DON FRANCISCO comienza con la actividad en Necochea en la comercialización  de maquinarias agrícolas y ganaderas en el año 2000.</p>
 							<p> Desde ese momento y hasta  la fecha se destaca como una de las principales en rubro en Necochea y la zona.</p>
-							<p><strong>Don Francisco Maquinarias</strong></p>
+							<p class="text-right"><strong>Don Francisco Maquinarias</strong></p>
 						</div>
 				</div>
 			</div>
@@ -53,10 +53,10 @@
 								<a class="contact" href="mailto:carinacools@donfranciscomaquinarias.com">carina@donfranciscomaquinarias.com</a>
 							</div>
 						</li>
-						<li class="space">
+						<li class="space facebook">
 							<div>
-									<i class="fa fa-facebook-official space" aria-hidden="true"></i>
-									<a class="contact" href="https://www.facebook.com/pages/Don-Francisco-Maquinarias/160607427288244?fref=ts" target="_blank">Facebook</a>
+								<i class="fa fa-facebook space" aria-hidden="true"></i>
+								<a class="contact" href="https://www.facebook.com/pages/Don-Francisco-Maquinarias/160607427288244?fref=ts" target="_blank">Facebook</a>
 							</div>
 						</li>
 						</ul>
@@ -68,11 +68,11 @@
 				<div class="row footer-bottom">
 					<div class="col-md-offset-2 col-md-4 col-xs-12">
 							<ul>
-								<li><a href="http://donfranciscomaquinarias.com/">Inicio</a></li>
-								<li><a href="http://donfranciscomaquinarias.com/nosotros.php">Nosotros</a></li>
-								<li><a href="http://donfranciscomaquinarias.com/ubicacion.php">Ubicación</a></li>
-								<li><a href="http://donfranciscomaquinarias.com/search.php">Nuevos y Usados</a></li>
-								<li><a href="http://donfranciscomaquinarias.com/contacto.php">Contacto</a></li>
+								<li><a href="index.php">Inicio</a></li>
+								<li><a href="nosotros.php">Nosotros</a></li>
+								<li><a href="ubicacion.php">Ubicación</a></li>
+								<li><a href="search.php">Nuevos y Usados</a></li>
+								<li><a href="contacto.php">Contacto</a></li>
 							</ul>
 					</div>
 						<div class="col-md-4 col-xs-12">copyright © 2013 <a href="http://www.realview-la.com/" target="_blank">Realview</a>

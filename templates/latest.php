05e572c6d87b818c1a9fca00302da49a667c996f
Silvina Muñoz <silmunoz22@gmail.com>
2017-12-02 16:11:54 -0300

    img en dos columnas y pag responsive #13

--- templates/latest.php
+++ templates/latest.php
@@ -36,7 +36,7 @@ $resultados =  $db->consulta('SELECT
 		<div class="container-fluid">
 			<div class="row">
 
-				<div class="col-xs-12 col-sm-10 col-md-10 col-md-offset-1">
+				<div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1">
 					<h4 class="section-title section-line">Usados Seleccionados</h4>
 
 
--- templates/ubicacion.php
+++ templates/ubicacion.php
@@ -5,34 +5,36 @@
 		<div class="col-xs-12  col-md-10 col-md-offset-1 col-sm-12">
 			<h4 class="section-line section-tittle">Ubicacion</h4>
 		</div>
-	<div class="col-xs-12 col-md-7 col-md-offset-1 col-sm-12">
-		<h4>Ruta 228 Km. 2, Necochea. Bs As</h4>
-		<iframe width="800" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=-38.532221,-58.769871&amp;num=1&amp;ie=UTF8&amp;t=h&amp;ll=-38.532187,-58.770504&amp;spn=0.030214,0.051413&amp;z=14&amp;output=embed"></iframe>
-		<figcaption><small><a href="https://maps.google.com/maps?q=-38.532221,-58.769871&amp;num=1&amp;ie=UTF8&amp;t=h&amp;ll=-38.532187,-58.770504&amp;spn=0.030214,0.051413&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small></figcaption>
+		<div class="col-xs-12 col-md-7 col-md-offset-1 col-sm-8">
+			<h4>Ruta 228 Km. 2, Necochea. Bs As</h4>
+			<iframe class="iframe-map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=-38.532221,-58.769871&amp;num=1&amp;ie=UTF8&amp;t=h&amp;ll=-38.532187,-58.770504&amp;spn=0.030214,0.051413&amp;z=14&amp;output=embed"></iframe>
+			<figcaption><small><a href="https://maps.google.com/maps?q=-38.532221,-58.769871&amp;num=1&amp;ie=UTF8&amp;t=h&amp;ll=-38.532187,-58.770504&amp;spn=0.030214,0.051413&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small></figcaption>
 
-<ul>
-	<li class="li-ubicacion">
-		<a href="files/donfrancisco.jpg">
-			<figure>
-				<img src="files/donfrancisco.jpg" class="img-ubicacion img-responsive">
-			</figure>
-		</a>
-		<figcaption><h6>Ruta 228 Km. 2, Necochea</h6></figcaption>
-	</li>
-	<li class="li-ubicacion">
-		<a href="files/buenosaires.jpg">
-		<figure>
-			<img src="files/buenosaires.jpg" class="img-ubicacion img-responsive" alt="" title="" style="border:#CCC 1px solid">
-		</figure>
-		</a>
-		<figcaption><h6>Necochea, Pcia de Buenos Aires</h6></figcaption>
-	</li>
-</ul>
-</div>
+			<div class="col-md-6 col-sm-6 col-xs-6">
 
-<div class="col-md-3 col-xs-12 col-sm-12">
-	<?php include('templates/sidebar_interior.php'); ?>
-</div>
-</div>
+					<a href="files/donfrancisco.jpg">
+						<figure>
+							<img src="files/donfrancisco.jpg" class="img-ubicacion img-responsive">
+						</figure>
+					</a>
+					<figcaption><h6>Ruta 228 Km. 2, Necochea</h6></figcaption>
+
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-6">
+						<a href="files/buenosaires.jpg">
+						<figure>
+							<img src="files/buenosaires.jpg" class="img-ubicacion img-responsive" alt="" title="" style="border:#CCC 1px solid">
+						</figure>
+					</a>
+					<figcaption><h6>Necochea, Pcia de Buenos Aires</h6></figcaption>
+
+				</div>
+
+		</div>
+
+		<div class="col-md-3 col-xs-12 col-sm-4">
+			<?php include('templates/sidebar_interior.php'); ?>
+		</div>
+	</div>
 
 </div>

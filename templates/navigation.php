89e4c5af5329ba90b351feb43183ea3ab9c4cfe1
Silvina Muñoz <silmunoz22@gmail.com>
2017-12-05 16:43:23 -0300

    fixed #15

--- templates/navigation.php
+++ templates/navigation.php
@@ -4,35 +4,30 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid" id="header">
     <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="row">
-          <div class="col-md-12 col-xs-12">
-
-    <div class="navbar-header">
-      <div class="col-md-6 col-md-offset-6 col-xs-10 col-xs-offset-2 ">
-
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-
-          <a class="navbar-brand" id="logo" href="index.php"><img class="img-responsive center-block" src="files/logo0000.png" alt="logo"></a>
-
+    <div class="row">
+      <div class="col-md-12 col-xs-12 col-sm-12">
+        <div class="navbar-header">
+          <div class="col-md-6 col-md-offset-6 col-xs-10 col-xs-offset-2 ">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" id="logo" href="index.php"><img class="img-responsive center-block" src="files/logo0000.png" alt="logo"></a>
+          </div>
         </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="nosotros.php">Nosotros</a></li>
+            <li><a href="ubicacion.php">Ubicación</a></li>
+            <li><a href="search.php">Nuevos y usados</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
       </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="nosotros.php">Nosotros</a></li>
-        <li><a href="ubicacion.php">Ubicación</a></li>
-        <li><a href="search.php">Nuevos y usados</a></li>
-        <li><a href="contacto.php">Contacto</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div>
-  </div>
+    </div>
   </div><!-- /.container-fluid -->
 </nav>
